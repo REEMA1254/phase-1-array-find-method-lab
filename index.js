289629c90dec651f6c6df300d46694c74c52b419
maryam <Reemaali9048@gmail.com>
2023-09-28 01:50:42 +0300

    complete assigment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(arrayOfObjects) {
+    const win = arrayOfObjects.find((game) => game.result === "W");
+    return win ? win.year : undefined;
+  }
+  const superBowlGames = [
+    { year: 2020, result: "L" },
+    { year: 2021, result: "W" },
+    { year: 2022, result: "L" },
+  ];
+  
+  const winningYear = superbowlWin(superBowlGames);
+  console.log(winningYear);
